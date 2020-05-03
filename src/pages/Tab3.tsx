27fecb07da8ee4b51a26a5cd1210da29d5b2f8e8
hanwiles <54829099+hanwiles@users.noreply.github.com>
2020-05-03 13:37:52 -0600

    Attempt to add searchbar
    Attempt to add a searchbar

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, {useState} from 'react';
+import { IonContent, IonHeader, IonFooter, IonPage, IonTitle, IonToolbar, IonSearchbar, IonCard} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import {IonicSelectableModule} from 'ionic-selectable';
+import ItemList from '../ItemList';
+import Item from '../item';
 
 const Tab3: React.FC = () => {
+
+  const[searchText, setSearchText] = useState('');
+  const [setCurrent] = useState(null);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Find Events</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent>    
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+              <IonTitle size="large">Tab 3</IonTitle>      
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonSearchbar value={searchText} onIonChange={e => 
+          setSearchText(e.detail.value!)} showCancelButton="focus"></IonSearchbar>
+        <IonCard>
+          <ItemList doEdit={setCurrent}/>
+        </IonCard>
       </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          Search Text: {searchText ?? '(none)'}
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
